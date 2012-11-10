1b63c05497dd923afe8d3d8049f31407ff9deec2
Pierre Bertet <bonjour@pierrebertet.net>
2012-11-10 03:17:25 +0100

    Basic structure

---
+++ client/index.js
@@ -0,0 +1,14 @@
+(function(window){
+  var document = window.document,
+      canv = document.getElementsByTagName('canvas')[0],
+      ctx = canv.getContext('2d'),
+      tileSize = 32;
+  
+  canv.width = tileSize * 25; // 800
+  canv.height = tileSize * 15; // 480
+  
+  function drawRoom(room) {
+    
+  }
+  
+})(this);
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+var http = require('http'),
+    fs = require('fs');
+
+http.createServer(function(req, res) {
+  
+  var urls = {
+    '/': 'tpl/index.html',
+    '/index.js': 'client/index.js'
+  };
+  
+  var send = function(filename, code) {
+    res.writeHead(code || 200, {'Content-Type': 'text/html'});
+    var fileStream = fs.createReadStream(__dirname + '/' + filename);
+    fileStream.pipe(res);
+  };
+  
+  for (var i in urls) {
+    if (req.url === i) {
+      return send(urls[i]);
+    }
+  }
+  
+  return send('tpl/404.html', 404);
+  
 }).listen(8000);
 
 console.log('Server running at http://0.0.0.0:8000/');
